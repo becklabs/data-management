50ef35c576c7010d1eb5d3537a3140673c7ed68e
unknown <becklabash@gmail.com>
2020-11-19 18:06:12 -0500

    update splitbyday

--- splitbyday.py
+++ splitbyday.py
@@ -11,7 +11,6 @@ def trackExtract(gps_filename):
         if 'esrignss_latitude' in list(gps_telem.columns):
             gps_telem = gps_telem.rename(columns={'esrignss_latitude': 'latitude', 'esrignss_longitude': 'longitude','esrignss_altitude':'elevation','esrignss_fixdatetime':'timestamp'})
             gps_telem1 = pd.DataFrame()
- 
             gps_telem1['latitude'] = gps_telem['latitude']
             gps_telem1['longitude'] = gps_telem['longitude']
             gps_telem1['elevation'] = gps_telem['elevation']
@@ -28,18 +27,20 @@ def trackExtract(gps_filename):
            gps_telem.loc[i,'timestamp'] = dateparser.parse(str(ts.year)+' '+str(ts.month)+' '+str(ts.day))
            i+=1
     return gps_telem
-df = trackExtract('Location_Tracking.csv')
-daylist = []
-for timestamp in df['timestamp']:
-    if str(timestamp) not in daylist:
-        daylist.append(str(timestamp))
-df = df.set_index(df['timestamp'])
-df = df.sort_index()
-if 'split_csvs' not in os.listdir():
-    os.mkdir('split_csvs/')
-for i in range(len(daylist)):
-    data = pd.DataFrame()
-    data = df[daylist[i]:daylist[i]]
-    data.to_csv('split_csvs/'+daylist[i][:10]+'.csv')
-#train = df['2015-01-10':'2016-12-20']
-#test  = df['2016-12-21':]
+
+def split_by_day(csv):
+    df = trackExtract(csv)
+    daylist = []
+    for timestamp in df['timestamp']:
+        if str(timestamp) not in daylist:
+            daylist.append(str(timestamp))   
+    df = df.set_index(df['timestamp'])
+    df = df.sort_index()
+    if 'split_csvs' not in os.listdir():
+        os.mkdir('split_csvs/')  
+    for i in range(len(daylist)):
+        data = pd.DataFrame()
+        data = df[daylist[i]:daylist[i]]
+        data.to_csv('split_csvs/'+daylist[i][:10]+'.csv')
+
+#split_by_day(csv)
